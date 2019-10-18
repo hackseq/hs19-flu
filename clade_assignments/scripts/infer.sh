6eed7e66e355859f613695699c0a6ad2f944b391
Frank Hui <huifrank@live.com>
2019-10-18 13:09:41 -0700

    Finish infer.sh

--- clade_assignments/scripts/infer.sh
+++ clade_assignments/scripts/infer.sh
@@ -1,6 +1,14 @@
-python3 -m augur ancestral --tree  --alignment aligned.fasta
---output /path/to/nt_muts.json --inference joint
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-B.fasta --output /path/to/INFERRED-B.json --inference joint --nthreads auto
 
-python3 -m augur ancestral --tree /path/to/flutree2018-5.nwk --alignment /path/to/aligned.fasta
---output /path/to/nt_muts.json --inference joint
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-BNA.fa.fasta --output /path/to/INFERRED-BNA.json --inference joint --nthreads auto
+
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H1N1.fasta --output /path/to/INFERRED-H1N1.json --inference joint --nthreads auto
+
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H1N1A.fasta --output /path/to/INFERRED-H1N1A.json --inference joint --nthreads auto
+
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H3N2.fasta --output /path/to/INFERRED-H3N2.json --inference joint --nthreads auto
+
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H3N2NA.fasta --output /path/to/INFERRED-H3N2NA.json --inference joint --nthreads auto
+
+python3 -m augur ancestral --tree clade_assignments/trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-PH3N2.fasta --output /path/to/INFERRED-PH3N2.json --inference joint --nthreads auto
 
