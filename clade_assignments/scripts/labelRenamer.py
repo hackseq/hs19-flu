9b9c74f6fb83dd304c66434db67c9e7ab49f6bc9
bricoletc <bletcher@ebi.ac.uk>
2019-10-18 22:00:33 +0100

    labelRenamer input param

--- clade_assignments/scripts/labelRenamer.py
+++ clade_assignments/scripts/labelRenamer.py
@@ -1,7 +1,14 @@
-fname = 'aligned.fasta'
-newfname = 'aligned_correct.fasta'
+import sys
+try:    
+    in_file = sys.argv[1]
+except IndexError:
+    print(f"Clean fasta labels. Usage: {sys.argv[0]} alignment.fa")
+    sys.exit(0)
 
-with open(fname, 'r') as f:
+prefix, postfix = in_file.split(".")[0], in_file.split(".")
+newfname = f"{prefix}_clean_labels{postfix}"
+
+with open(in_file, 'r') as f:
 	with open(newfname, 'w') as fout:
 		for line in f:
 			sline = line.strip()
