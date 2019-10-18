0a50b1c40dd8ecd3fe38b7d22692a61fb74a469b
Frank Hui <huifrank@live.com>
2019-10-18 12:50:18 -0700

    Move tree files to tree folder

---
+++ infer.sh
@@ -0,0 +1,6 @@
+python3 -m augur ancestral --tree  --alignment aligned.fasta
+--output /path/to/nt_muts.json --inference joint
+
+python3 -m augur ancestral --tree /path/to/flutree2018-5.nwk --alignment /path/to/aligned.fasta
+--output /path/to/nt_muts.json --inference joint
+
