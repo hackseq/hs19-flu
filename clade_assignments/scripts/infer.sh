1b69bad559df001479aeb14fb691c9e7e98b2cbb
zhamadeh <zeid.hamadeh@hotmail.com>
2019-10-18 17:17:44 -0700

    t

--- clade_assignments/scripts/infer.sh
+++ clade_assignments/scripts/infer.sh
@@ -1,14 +1,26 @@
-python3 -m augur ancestral --tree ../trees/flutreeB_HA.tree --alignment ../../ali/ALIGNED-B.fa --output ../../infer/INFERRED-B.json --inference joint 
+#need to run these lines beforehand
+#python3 -m augur refine --tree ../trees/flutreeB_HA.nwk --output-tree ../trees/flutreeB_HA_2.nwk
+#python3 -m augur refine --tree ../trees/flutreeH1N1_HA.nwk --output-tree ../trees/flutreeH1N1_HA_2.nwk
+#python3 -m augur refine --tree ../trees/flutreeH3N2_HA.nwk --output-tree ../trees/flutreeH3N2_HA_2.nwk
 
-python3 -m augur ancestral --tree ../trees/flutreeB_HA.tree --alignment ../../ali/ALIGNED-BNA.fa --output ../../infer/INFERRED-BNA.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H1N1.fa --output ../../infer/INFERRED-H1N1.json --inference joint 
+python3 -m augur ancestral --tree ../trees/flutreeB_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-B_clean_labels.fa --output ../../infer/INFERRED-B.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA.tree --alignment ../../ali/ALIGNED-H1N1A.fa --output ../../infer/INFERRED-H1N1A.json --inference joint
 
-python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA.tree --alignment ../../ali/ALIGNED-H3N2.fa --output ../../infer/INFERRED-H3N2.json --inference joint
+#python3 -m augur ancestral --tree ../trees/flutreeB_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-BNA_clean_labels.fa --output ../../infer/INFERRED-BNA.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA.tree --alignment ../../ali/ALIGNED-H3N2NA.fa --output ../../infer/INFERRED-H3N2NA.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA.tree --alignment ../../ali/ALIGNED-PH3N2.fa --output ../../infer/INFERRED-PH3N2.json --inference joint 
+#python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-H1N1_clean_labels.fa --output ../../infer/INFERRED-H1N1.json --inference joint 
+
+
+#python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-H1N1A_clean_labels.fa --output ../../infer/INFERRED-H1N1A.json --inference joint
+
+
+
+#python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-H3N2_clean_labels.fa --output ../../infer/INFERRED-H3N2.json --inference joint
+
+#python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-H3N2NA_clean_labels.fa --output ../../infer/INFERRED-H3N2NA.json --inference joint 
+
+
+#python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA_2.nwk --alignment ../hackseq/ali/ALIGNED-PH3N2_clean_labels.fa --output ../../infer/INFERRED-PH3N2.json --inference joint 
 
