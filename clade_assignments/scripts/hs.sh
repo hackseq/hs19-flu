7988af2f69d4edeab2935ec81d266156b1f98d1a
ivan <ivan@Notetakings-MacBook-Pro.local>
2019-10-18 17:02:05 -0700

    Added more power to hs.sh script

--- clade_assignments/scripts/hs.sh
+++ clade_assignments/scripts/hs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 -m augur align --sequences hackseq/fas/alignB.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_yam_ha.gb --output hackseq/ali/ALIGNED-B.fa
-python3 -m augur align --sequences hackseq/fas/alignBNA.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-BNA.fa
-python3 -m augur align --sequences hackseq/fas/alignH1N1.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-H1N1.fa
-python3 -m augur align --sequences hackseq/fas/alignH1N1NA.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-H1N1NA.fa
-python3 -m augur align --sequences hackseq/fas/alignH3N2.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-H3N2.fa
-python3 -m augur align --sequences hackseq/fas/alignH3N2NA.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-H3N2NA.fa
-python3 -m augur align --sequences hackseq/fas/PH3N2.fa --fill-gaps --nthreads 20 --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-PH3N2.fa
+python3 -m augur align --sequences hackseq/fas/alignB.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_yam_ha.gb --output hackseq/ali/ALIGNED-B.fa
+python3 -m augur align --sequences hackseq/fas/alignBNA.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-BNA.fa
+python3 -m augur align --sequences hackseq/fas/alignH1N1.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-H1N1.fa
+python3 -m augur align --sequences hackseq/fas/alignH1N1NA.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h1n1pdm_ha.gb --output hackseq/ali/ALIGNED-H1N1NA.fa
+python3 -m augur align --sequences hackseq/fas/alignH3N2.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-H3N2.fa
+python3 -m augur align --sequences hackseq/fas/alignH3N2NA.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-H3N2NA.fa
+python3 -m augur align --sequences hackseq/fas/PH3N2.fa --fill-gaps --nthreads auto --reference-sequence hackseq/ref/reference_h3n2_ha.gb --output hackseq/ali/ALIGNED-PH3N2.fa
