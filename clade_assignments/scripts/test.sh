db009916fb65e9a93fc4b48a64f76fb9a87570d1
zhamadeh <zeid.hamadeh@hotmail.com>
2019-10-19 10:41:47 -0700

    update

--- clade_assignments/scripts/test.sh
+++ clade_assignments/scripts/test.sh
@@ -1,1 +1,2 @@
-python3 -m augur ancestral --tree ../trees/flutreeB_HA.nwk --alignment ../../ali/ALIGNED-B_clean_labels.fa --output ../../infer/INFERRED-B.json --inference joint 
+python3 -m augur refine --tree ../trees/flutreeB_HA.nwk --output-tree ../trees/flutreeB_HA_2.nwk
+python3 -m augur ancestral --tree ../trees/flutreeB_HA_2.nwk --alignment ../../ali/ALIGNED-B_clean_labels.fa --output ../../infer/INFERRED-B.json --inference joint 
