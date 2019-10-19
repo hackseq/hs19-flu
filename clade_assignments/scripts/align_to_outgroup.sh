6323a659dfe3909c0aec0fececf2412c570d6cbc
Frank Hui <huifrank@live.com>
2019-10-19 10:22:34 -0700

    Delete unused files

--- clade_assignments/scripts/align_to_outgroup.sh
+++ clade_assignments/scripts/align_to_outgroup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-python3 -m augur align --sequences fas/alignB.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_yam_ha.gb --output ali/ALIGNED-B.fa
-python3 -m augur align --sequences fas/alignBNA.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h1n1pdm_ha.gb --output ali/ALIGNED-BNA.fa
-python3 -m augur align --sequences fas/alignH1N1.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h1n1pdm_ha.gb --output ali/ALIGNED-H1N1.fa
-python3 -m augur align --sequences fas/alignH1N1NA.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h1n1pdm_ha.gb --output ali/ALIGNED-H1N1NA.fa
-python3 -m augur align --sequences fas/alignH3N2.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h3n2_ha.gb --output ali/ALIGNED-H3N2.fa
-python3 -m augur align --sequences fas/alignH3N2NA.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h3n2_ha.gb --output ali/ALIGNED-H3N2NA.fa
-python3 -m augur align --sequences fas/PH3N2.fa --fill-gaps --nthreads auto --reference-sequence ref/reference_h3n2_ha.gb --output ali/ALIGNED-PH3N2.fa
+python3 -m augur align --sequences ../../Data/B.fa --fill-gaps --nthreads auto --reference-sequence ../references/reference_yam_ha.gb --output ../alignments/ALIGNED-B_yam.fa
+python3 -m augur align --sequences ../../Data/B.fa --fill-gaps --nthreads auto --reference-sequence ../references/reference_vic_ha.gb --output ../alignments/ALIGNED-B_vic.fa
+python3 -m augur align --sequences ../../Data/H1N1.fa --fill-gaps --nthreads auto --reference-sequence ../references/reference_h1n1pdm_ha.gb --output ../alignments/ALIGNED-H1N1.fa
+python3 -m augur align --sequences ../../Data/H3N2.fa --fill-gaps --nthreads auto --reference-sequence ../references/reference_h3n2_ha.gb --output ../alignments/ALIGNED-H3N2.fa
