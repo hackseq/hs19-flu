810d35d086728dfdd2640718616cd90830495f26
zhamadeh <zeid.hamadeh@hotmail.com>
2019-10-19 11:14:16 -0700

    new infer script

--- clade_assignments/scripts/infer.sh
+++ clade_assignments/scripts/infer.sh
@@ -4,12 +4,12 @@
 #python3 -m augur refine --tree ../trees/flutreeH3N2_HA.nwk --output-tree ../trees/flutreeH3N2_HA_2.nwk
 
 
-python3 -m augur ancestral --tree ../trees/flutreeB_HA_2.nwk --alignment ../alignments/ALIGNED-B_yam_clean_labels.fa --output ../../infer/INFERRED-B_yam.json --inference joint 
+python3 -m augur ancestral --tree ../trees/flutreeB_HA.nwk --alignment ../alignments/ALIGNED-B_yam_clean_labels.fa --output ../../infer/INFERRED-B_yam.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeB_HA_2.nwk --alignment ../alignments/ALIGNED-B_vic_clean_labels.fa --output ../../infer/INFERRED-B_vic.json --inference joint 
+python3 -m augur ancestral --tree ../trees/flutreeB_HA.nwk --alignment ../alignments/ALIGNED-B_vic_clean_labels.fa --output ../../infer/INFERRED-B_vic.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA_2.nwk --alignment ../alignments/ALIGNED-H1N1_clean_labels.fa --output ../../infer/INFERRED-H1N1.json --inference joint 
+python3 -m augur ancestral --tree ../trees/flutreeH1N1_HA.nwk --alignment ../alignments/ALIGNED-H1N1_clean_labels.fa --output ../../infer/INFERRED-H1N1.json --inference joint 
 
-python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA_2.nwk --alignment ../alignments/ALIGNED-H3N2_clean_labels.fa --output ../../infer/INFERRED-H3N2.json --inference joint
+python3 -m augur ancestral --tree ../trees/flutreeH3N2_HA.nwk --alignment ../alignments/ALIGNED-H3N2_clean_labels.fa --output ../../infer/INFERRED-H3N2.json --inference joint
 
 
