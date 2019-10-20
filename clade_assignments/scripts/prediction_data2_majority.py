8851f1f5837345e8f18fb3cd8afc6c9165016a16
Ivan <ivan.pmartell@hotmail.com>
2019-10-20 13:50:17 -0700

    Final results

--- clade_assignments/scripts/prediction_data2_majority.py
+++ clade_assignments/scripts/prediction_data2_majority.py
@@ -28,9 +28,12 @@ for pred_row in tqdm(predictions.iterrows(),total=len(predictions)):
         specific_clades = who_clade['cladesSpec'].real[0].split(',')
         for specific_clade in specific_clades:
             add_tip(specific_clade, total_tips)
-    representative_clade = max(total_tips.items(), key=operator.itemgetter(1))[0]
-    list_representatives.append([clade, representative_clade, pred_label])
+    max_who_clades = [x for i, x in enumerate(total_tips.items()) if x == max(total_tips.items(), key=operator.itemgetter(1))]
+    sum_who_clade = sum(total_tips.values())
+    percent_common = [[i, x / sum_who_clade] for i, x in total_tips.items()]
+    representative_clade = [x[0] for x in percent_common if x[1] >= 0.4] # THRESHOLD
+    list_representatives.append([clade, representative_clade, percent_common, pred_label])
 
-out_df = pd.DataFrame(list_representatives, columns=['subtree', 'WHO_max', 'pred'])
+out_df = pd.DataFrame(list_representatives, columns=['subtree', 'WHO_max', 'percent_common', 'pred'])
 out_df['subtree'] = out_df['subtree'].astype(int)
-out_df.to_csv('/Users/ivan/Documents/repos/hs19-flu/visualization/result3.csv')
+out_df.to_csv('/Users/ivan/Documents/repos/hs19-flu/visualization/result4.csv')
